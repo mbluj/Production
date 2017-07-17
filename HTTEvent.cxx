47b795d17b1aae1373e85accf93c8e448f323214
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2017-07-17 10:14:28 +0200

    * move energy scale factors to static HTTParticle class members * move calculation of the nominal energy scale shift to a method * apply systematic scale variations on top of the nominal shifts

--- HTTEvent.cxx
+++ HTTEvent.cxx
@@ -66,65 +66,60 @@ void HTTParticle::clear(){
 }
 ////////////////////////////////////////////////
 ////////////////////////////////////////////////
-const TLorentzVector & HTTParticle::getSystScaleP4(HTTAnalysis::sysEffects type) const{
-
+const TLorentzVector & HTTParticle::getNominalShiftedP4() const{
+  
   //Corrections of nominal tau-scale: https://twiki.cern.ch/twiki/bin/view/CMS/TauIDRecommendation13TeV#Tau_energy_scale
   float TES_1p=-0.005, TES_1ppi0=0.011, TES_3p=0.006;
-  if(type==HTTAnalysis::NOMINAL) {
-    lastSystEffect = type;
-    //correct nominal scale of genuine taus
-    if(std::abs(getPDGid())==15 && getProperty(PropertyEnum::mc_match)==5){
-      int dm = getProperty(PropertyEnum::decayMode);
-      if(dm==0) //1-prong
-	return getShiftedP4(1+TES_1p,true);
-      else if(dm==1 || dm==2) //1-prong+pi0s
-	return getShiftedP4(1+TES_1ppi0,false);
-      else if(dm==10) //3-prongs
-	return getShiftedP4(1+TES_3p,false);
-      else //others
-	return p4;
+
+  //correct nominal scale of genuine taus
+  if(std::abs(getPDGid())==15 && getProperty(PropertyEnum::mc_match)==5){
+    int dm = getProperty(PropertyEnum::decayMode);
+    if(dm==0) //1-prong
+      return getShiftedP4(1+TES_1p,true);
+    else if(dm==1 || dm==2) //1-prong+pi0s
+      return getShiftedP4(1+TES_1ppi0,false);
+    else if(dm==10) //3-prongs
+      return getShiftedP4(1+TES_3p,false);
+    else //others
+      return p4;
     }
-    return p4;
-  }
+    else return p4;
+}
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+const TLorentzVector & HTTParticle::getSystScaleP4(HTTAnalysis::sysEffects type) const{
+
+  if(type==HTTAnalysis::NOMINAL) return getNominalShiftedP4();          
   else if(lastSystEffect==type) return p4Cache;
 
   lastSystEffect = type;
 
   if(std::abs(getPDGid())==15 && getProperty(PropertyEnum::mc_match)==5){
-    ///True taus
-    
-    if(type!=HTTAnalysis::TESUp && type!=HTTAnalysis::TESDown) {
-      if(type==HTTAnalysis::DUMMY_SYS)
-	return p4;//return RAW uncorrected p4. FIXME: is it correct or DUMMY_SYS is sometimes used
-      else{//apply nominal corrections as above, ugly code duplication
-	int dm = getProperty(PropertyEnum::decayMode);
-	if(dm==0) //1-prong
-	  return getShiftedP4(1+TES_1p,true);
-	else if(dm==1 || dm==2) //1-prong+pi0s
-	  return getShiftedP4(1+TES_1ppi0,false);
-	else if(dm==10) //3-prongs
-	  return getShiftedP4(1+TES_3p,false);
-	else //others
-	  return p4;	
-      }
-    }
-    float TES = 0.012;
-    if(type==HTTAnalysis::TESDown) TES*=-1;
-    return getShiftedP4(1+TES,getProperty(PropertyEnum::decayMode)==0);
+    ///True taus    
+    if(type!=HTTAnalysis::TESUp && type!=HTTAnalysis::TESDown) return getNominalShiftedP4();    
+    float direction = 1;
+    if(type==HTTAnalysis::TESDown) direction*=-1;
+    float nominalShift = 1.0;
+    int dm = getProperty(PropertyEnum::decayMode);
+    if(dm==0) nominalShift = 1+TES_1p;
+    else if(dm==1 || dm==2) nominalShift = 1+TES_1ppi0;
+    else if(dm==10) nominalShift = 1+TES_3p;
+    float shift = nominalShift*(1+direction*TES);
+    return getShiftedP4(shift,getProperty(PropertyEnum::decayMode)==0);
   }
   if(std::abs(getPDGid())==15 && getProperty(PropertyEnum::mc_match)==3){
     ///Fake e->tau
     if(type!=HTTAnalysis::E2TUp && type!=HTTAnalysis::E2TDown) return p4;
-    float EES = 0.03;
-    if(type==HTTAnalysis::E2TDown) EES*=-1;
-    return getShiftedP4(1+EES,getProperty(PropertyEnum::decayMode)==0);
+    float direction = 1;
+    if(type==HTTAnalysis::E2TDown) direction*=-1;
+    return getShiftedP4(1+direction*EES,getProperty(PropertyEnum::decayMode)==0);
   }
   if(std::abs(getPDGid())==15 && getProperty(PropertyEnum::mc_match)==4){
     ///Fake mu->tau
     if(type!=HTTAnalysis::M2TUp && type!=HTTAnalysis::M2TDown) return p4;
-    float MES = 0.03;
-    if(type==HTTAnalysis::M2TDown) MES*=-1;
-    return getShiftedP4(1+MES,getProperty(PropertyEnum::decayMode)==0);
+    float direction = 1;
+    if(type==HTTAnalysis::M2TDown) direction*=-1;
+    return getShiftedP4(1+direction*MES,getProperty(PropertyEnum::decayMode)==0);
   }
   if(std::abs(getPDGid())==98){
     if(type!=HTTAnalysis::JESUp && type!=HTTAnalysis::JESDown) return p4;
