750dfd1ae9e620cc8183bb37709ae9e8b67148e9
Michal Bluj <michal.bluj@cern.ch>
2017-10-04 12:48:45 +0200

    updated list of datasets as for 4.10.2017

--- datasetsSummer17.py
+++ datasetsSummer17.py
@@ -1,21 +1,51 @@
 datasets = [
-   #Data
+    #Data
+    #"/SingleMuon/Run2017A-PromptReco-v2/MINIAOD",
+    #"/SingleMuon/Run2017A-PromptReco-v3/MINIAOD",
     "/SingleMuon/Run2017B-PromptReco-v1/MINIAOD",
     "/SingleMuon/Run2017B-PromptReco-v2/MINIAOD",
     "/SingleMuon/Run2017C-PromptReco-v1/MINIAOD",
     "/SingleMuon/Run2017C-PromptReco-v2/MINIAOD",
+    "/SingleMuon/Run2017C-PromptReco-v3/MINIAOD",
+    "/SingleMuon/Run2017D-PromptReco-v1/MINIAOD",
+    "/SingleMuon/Run2017E-PromptReco-v1/MINIAOD",
     ####
+    #"/Tau/Run2017A-PromptReco-v1/MINIAOD",
+    #"/Tau/Run2017A-PromptReco-v2/MINIAOD",
+    #"/Tau/Run2017A-PromptReco-v3/MINIAOD",
     "/Tau/Run2017B-PromptReco-v1/MINIAOD",
     "/Tau/Run2017B-PromptReco-v2/MINIAOD",
     "/Tau/Run2017C-PromptReco-v1/MINIAOD",
     "/Tau/Run2017C-PromptReco-v2/MINIAOD",
+    "/Tau/Run2017C-PromptReco-v3/MINIAOD",
+    "/Tau/Run2017D-PromptReco-v1/MINIAOD",
+    "/Tau/Run2017E-PromptReco-v1/MINIAOD",
     #Signal SM
-    #Signal MSSM
-    #DY
-    #Wjets
+    "/GluGluHToTauTau_M125_13TeV_powheg_pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v2/MINIAODSIM",
+    "/VBFHToTauTau_M125_13TeV_powheg_pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v2/MINIAODSIM",
+    #Signal MSSM (only mass closest to 125GeV for CP)
+    "/SUSYGluGluToBBHToTauTau_M-140_TuneCUETP8M1_13TeV-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v2/MINIAODSIM",
+    "/SUSYGluGluToHToTauTau_M-140_TuneCUETP8M1_13TeV-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v2/MINIAODSIM",
+    #DY (only inclisive)
+    "/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10_ext1-v2/MINIAODSIM", #use v2, v1 is incorrect?
+    #"/DY1JetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v1/MINIAODSIM",
+    #"/DY2JetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v1/MINIAODSIM",
+    #"/DY3JetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v2/MINIAODSIM",
+    #"/DY4JetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v2/MINIAODSIM",
+    #Wjets (only inclisive)
+    "/WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v1/MINIAODSIM",
+    #"/W1JetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v1/MINIAODSIM",
+    #"/W2JetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v1/MINIAODSIM",
+    #"/W3JetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v1/MINIAODSIM",
+    #"/W4JetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v1/MINIAODSIM",
     #TT
+    "/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10_ext1-v1/MINIAODSIM",
     ##DY low M
+    "/DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v1/MINIAODSIM",
     ##VV
+    #"/WZ_TuneCUETP8M1_13TeV-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v2/MINIAODSIM",
+    #"/WW_TuneCUETP8M1_13TeV-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v2/MINIAODSIM",
+    #"/ZZ_TuneCUETP8M1_13TeV-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v2/MINIAODSIM",
     ###VVV
     ###EWK+2jets
     ##QCD
